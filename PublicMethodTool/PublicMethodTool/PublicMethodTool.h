f3dcacfb7078acfdb8f5c35c30bf44dc6442d9ad
深大水哥 <393063570@qq.com>
2016-06-01 14:09:13 +0800

    增加方法申明

--- PublicMethodTool/PublicMethodTool/PublicMethodTool.h
+++ PublicMethodTool/PublicMethodTool/PublicMethodTool.h
@@ -15,4 +15,36 @@
  */
 @interface PublicMethodTool : NSObject
 
++ (CGFloat)getFileSize:(NSString *)filePath;
++ (CGFloat)diskOfAllSizeMBytes;
++ (CGFloat)diskOfFreeSizeMBytes;
++ (NSString *)firstCharacterWithString:(NSString *)string;
++ (NSDictionary *)dictionaryOrderByCharacterWithOriginalArray:(NSArray *)array;
++ (NSString *)currentDateWithFormat:(NSString *)format;
++ (NSString *)timeIntervalFromLastTime:(NSString *)lastTime
+                        lastTimeFormat:(NSString *)format1
+                         ToCurrentTime:(NSString *)currentTime
+                     currentTimeFormat:(NSString *)format2;
++ (NSString *)timeIntervalFromLastTime:(NSDate *)lastTime ToCurrentTime:(NSDate *)currentTime;
++ (UIColor *)colorWithHexString:(NSString *)color;
++ (UIImage *)filterWithOriginalImage:(UIImage *)image filterName:(NSString *)name;
++ (UIImage *)blurWithOriginalImage:(UIImage *)image blurName:(NSString *)name radius:(NSInteger)radius;
++ (UIImage *)colorControlsWithOriginalImage:(UIImage *)image
+                                 saturation:(CGFloat)saturation
+                                 brightness:(CGFloat)brightness
+                                   contrast:(CGFloat)contrast;
++ (UIVisualEffectView *)effectViewWithFrame:(CGRect)frame;
++ (UIImage *)shotScreen;
++ (UIImage *)shotWithView:(UIView *)view;
++ (UIImage *)shotWithView:(UIView *)view scope:(CGRect)scope;
++ (UIImage *)compressOriginalImage:(UIImage *)image toSize:(CGSize)size;
++ (NSData *)compressOriginalImage:(UIImage *)image toMaxDataSizeKBytes:(CGFloat)size;
++ (NSString *)getIPAddress;
++ (BOOL)isHaveSpaceInString:(NSString *)string;
++ (BOOL)isHaveString:(NSString *)string1 InString:(NSString *)string2;
++ (BOOL)isAllNum:(NSString *)string;
++ (UIView *)createDashedLineWithFrame:(CGRect)lineFrame
+                           lineLength:(int)length
+                          lineSpacing:(int)spacing
+                            lineColor:(UIColor *)color;
 @end
